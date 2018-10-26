c7a097433a3fe6de1494eb78b0aa06b5fa2f8537
SalimTerryLi <lhf2613@gmail.com>
2018-10-26 21:06:08 +0800

    homework commit.#1

--- equation.c
+++ equation.c
@@ -2,3 +2,25 @@
 //ax + by = c
 //dx + ey = f
 //注意输入/输出，数据类型
+
+#include <stdio.h>
+
+int main(int argc,char **argv) {
+    int a,b,c;
+    int d,e,f;
+    int ret;
+    printf("ax + by = c\ndx + ey = f\n");
+    fflush(stdin);
+    if(scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f)-6)
+        return -1;
+    long A=a*e-b*d;
+    if (A){
+        long D1=a*f-c*d;
+        long D2=c*e-b*f;
+        printf("%dx + %dy = %d\n%dx + %dy = %d\n",a,b,c,d,e,f);
+        printf("x=%lf y=%lf\n",(double)D2/(double)A,(double)D1/(double)A);
+        return 0;
+    }
+    printf("no real roots or too many roots.\n");
+    return 0;
+}
--- equation1.c
+++ equation1.c
@@ -1,3 +1,24 @@
 //求解一元二次方程的根
 //查阅sqrt()函数
 //注意有没有解
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int A,B,C;
+    printf("Ax + By + C = 0\n");
+    if(scanf("%d %d %d",&A,&B,&C)-3)
+        return -1;
+    long delta=pow(B,2) - 4*A*C;
+    if (delta<0){
+        printf("no real roots.\n");
+    }
+    else if (delta==0) {
+        printf("x=%lf\n",(double)-B/(2*(double)A));
+    }
+    else {
+        printf("x1=%lf x2=%lf\n",((double)-B+sqrt(delta))/(2*(double)A),((double)-B-sqrt(delta))/(2*(double)A));
+    }
+    return 0;
+}
